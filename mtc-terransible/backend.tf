70ef1584e3f8ca7e624c1a35108491376208db45
Busi <pietersonbusi@gmail.com>
2023-06-30 19:17:39 +0200

    files added

---
+++ mtc-terransible/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    organization = "mtc-terransible-project"
+
+    workspaces {
+      name = "terransible-devops"
+    }
+  }
+}
---
+++ mtc-terransible/variables.tf
@@ -0,0 +1,9 @@
+variable vpc_cidr {
+    type = string
+    default = "10.124.0.0/16"
+}
+
+variable access_ip {
+    type = string
+    default = "0.0.0.0/0"
+}
